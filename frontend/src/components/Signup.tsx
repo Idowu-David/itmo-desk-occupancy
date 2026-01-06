0bc0c6f717c3713cdf1b2f7f7cc129844891bf85
Idowu-David <Idowu-David@github.com>
2026-01-06 15:00:43 +0100

    welcome-back: mobile view

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,3 +1,7 @@
+import { FcGoogle } from "react-icons/fc";
+import { FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Signup = () => {
   return (
     <div className="bg-white flex flex-col items-center justify-center h-svh">
@@ -5,30 +9,39 @@ const Signup = () => {
         <h1 className="text-[50px] font-medium text-center text-[#020617]">
           Welcome Back!
         </h1>
-        <p className="text-gray-600 mt-4 text-xl">
-          Please log in to continue
-        </p>
+        <p className="text-gray-600 mt-4 text-xl">Please log in to continue</p>
         <div className="space-y-5 w-full mt-14">
-          <button className="bg-[#F1F5F9] py-3 w-full text-center rounded-md font-semibold">
-            Continue with Google
+          <button className="bg-[#F1F5F9] py-3 w-full text-center rounded-md font-semibold flex gap-3 items-center justify-center transition-colors hover:bg-gray-300">
+            <FcGoogle className="text-xl" />
+            <span className="font-medium text-[#0F172A]">
+              Continue with Google
+            </span>
           </button>
-          <button className="bg-[#F1F5F9] py-3 w-full text-center rounded-md font-semibold">
-            Continue with Linkedln
+
+          <button className="bg-[#F1F5F9] py-3 w-full text-center rounded-md font-semibold flex gap-3 items-center justify-center transition-colors hover:bg-gray-300">
+            <FaLinkedin className="text-xl text-[#0077B5]" />
+            <span className="font-medium text-[#0F172A]">
+              Continue with Linkedln
+            </span>
           </button>
         </div>
-        <p className="text-center font-bold text-xl my-6">OR</p>
-        <form className="flex flex-row gap-1 w-[98px] justify-center items-center min-h-20">
+        <div className="flex items-center w-full my-6">
+          <div className="flex-1 h-px bg-gray-200"></div>
+          <span className="px-4 text-gray-900 font-medium">OR</span>
+          <div className="flex-1 h-px bg-gray-200"></div>
+        </div>
+        <form className="flex flex-row gap-2 justify-center items-center min-h-20 w-4/5">
           <input
             placeholder="Enter your email address"
-            className="pl-4 rounded-md h-12 border border-[#E2E8F0]"
+            className="pl-4 rounded-md h-12 border border-[#E2E8F0] w-full max-w-md"
           />
           <button className="bg-[#2C5CC5] text-white rounded-md h-12 px-4">
             Continue
           </button>
         </form>
-        <p className="text-lg mt-10 text-[#64748B]">
-          Don't have an account?
-          <span className="text-[#2C5CC5]"> Sign up</span>
+        <p className="text-lg mt-8 text-[#64748B]">
+          Don't have an account? {" "}
+          <Link to="/signup" className="text-[#2C5CC5] underline"> Sign up</Link>
         </p>
       </div>
     </div>
@@ -49,8 +62,6 @@ const Signup = () => {
 // height: 512px;
 // left: calc(50% - 328px/2);
 // top: calc(50% - 512px/2);
-
-
 
 // background: #2C5CC5;
 // /* shadows/shadow-lg
